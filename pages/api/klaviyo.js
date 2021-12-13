0bf76657ba68d08ee738c64d4a6610fe9e872145
Boopathi Rajaa <me@boopathi.in>
2021-12-13 14:42:30 +0100

    fix build

--- pages/api/klaviyo.js
+++ pages/api/klaviyo.js
@@ -1,4 +1,4 @@
-export default async (req, res) => {
+export default async function handler(req, res) {
   const { email } = req.body
   if (!email) {
     return res.status(400).json({ error: 'Email is required' })
